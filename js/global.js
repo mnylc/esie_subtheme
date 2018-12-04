7c6735769050a8d8e2fe207db5bef8041adbc2a3
Diego Pino Navarro <dpino@metro.org>
2018-12-04 13:39:51 -0500

    First public push
    Not much to see right now

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.archipelago_subtheme = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
