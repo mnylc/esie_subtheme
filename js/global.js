00651ebb5a6b90468ba10c8b82336e5edbbf9aa8
Diego Pino Navarro <dpino@metro.org>
2019-04-25 16:38:27 -0400

    Trying to bring demo theme in order
    - makes menu collapse using defined breakpoints
    - Converts flex rows into columns for collapsed items
    - A lot of clean up and simpler colores (on the cooler side of the color wheel)
    - Fixes Advanced Form in edit and add Digital Objects

--- js/global.js
+++ js/global.js
@@ -9,8 +9,14 @@
 
   Drupal.behaviors.archipelago_subtheme = {
     attach: function (context, settings) {
-
+        $(window).on('resize', function() {
+          // de-collapses if collapsed and media query restores large viewport.
+          var MOBILE_WIDTH = 992;
+            if ($(window).width() >= MOBILE_WIDTH) {
+              $('.navbar-collapse.collapse.show').removeClass('show');
+            }
+        });
     }
-  };
+  }
 
 })(jQuery, Drupal);
