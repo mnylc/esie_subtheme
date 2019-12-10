e10116c09d8322349f1dbb4c3670184174f254b8
Marlo Longley <marlo-longley@users.noreply.github.com>
2019-12-10 06:34:14 -0800

    ISSUE-1: Style adjustments for ESIE (#8)
    * Add style and modify template for multiple menus in Navbar
    
    * Remove slashes before page titles (design element)
    
    * Add favicon to base theme folder; Fixes issue with not showing to logged out users
    
    * Make sure content is offset by the height of admin toolbar

--- js/global.js
+++ js/global.js
@@ -16,6 +16,16 @@
               $('.navbar-collapse.collapse.show').removeClass('show');
             }
         });
+        
+      // Triggers admin toolbar offset on page load    
+      if (context === document) {
+        $(context).on("drupalViewportOffsetChange.toolbar", function (event, offsets) {
+            let $body = $("body");
+            if ($body.length > 0 && offsets.top > 0 && $body.css('padding-top') !== offsets.top) {
+              $body.css("padding-top", offsets.top);
+            }
+        });
+      }
     }
   }
 
